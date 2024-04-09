886062dea0f1b9a8940ed8f6dad08a3f4bd95076
EoinBrennan2000 <eoinbrennan2000@gmail.com>
2024-04-09 22:18:48 +0100

    Base Functions Complete
    The resolution and draw speed sliders have been added as well as a toggle to show/hide the path points.
    Baseline functionality for displaying is complete, will now add pushing to the system

--- SisyphusBuild/UserDevice/UserControl.py
+++ SisyphusBuild/UserDevice/UserControl.py
@@ -21,13 +21,16 @@ lineThickness = 4
 
 arms = []       # List for arms to be stored in
 drawPath = []   # List for the drawn path to be stored in
+colArray = []
 
-buffSize = 10    # Size of buffer for number of arms drawn
+buffSize = 5    # Size of buffer for number of arms drawn
 
 startColour = [136,64,66]   # "#05743C" in RGB Decimal Values
 endColour = [29,36,66]   # "#3BE490" in RGB Decimal Values
 
 drawTime = 0.001
+
+pointHide = False
 
 ## tkinter setup
 
@@ -39,53 +42,61 @@ border = 25
 ## Classes
 
 class armStructure:
-    def __init__(self, armBase, armJoint, midOval, colour, buffNum):
-        self.armBase = armBase
-        self.armJoint = armJoint
-        self.midOval = midOval
-        self.colour = colour
-        self.buffNum = buffNum
+    def __init__(self, endx, endy, midx, midy, colourIndex):
+        self.endx = endx; self.endy = endy
+        self.midx = midx; self.midy = midy
 
+        self.armJoint = canvas.create_line(endx, endy, midx, midy, width =lineThickness-1, fill = colArray[colourIndex])
+        self.armBase = canvas.create_line(midx, midy, visualiserArea/2, visualiserArea/2, width =lineThickness-1, fill = colArray[colourIndex])
+        self.centre = DrawPoint(midx, midy, 0.5)
+        
     def undraw(self):
         canvas.delete(self.armBase)
         canvas.delete(self.armJoint)
-        canvas.delete(self.midOval)
+        canvas.delete(self.centre)
 
-    def replace(self, newArmBase, newArmJoint, newMidOval, buffVal):
-        self.undraw()
-        self.armBase = newArmBase
-        self.armJoint = newArmJoint
-        self.midOval = newMidOval
-        self.buffNum = buffVal
-
-    def recolour(self, colour):
-        canvas.itemconfigure(self.armBase, fill = colour)
-        canvas.itemconfigure(self.armJoint, fill = colour)
+    def recolour(self, colourIndex):
+        canvas.itemconfigure(self.armJoint, fill = colArray[colourIndex])
+        canvas.itemconfigure(self.armBase, fill = colArray[colourIndex])
+        canvas.itemconfigure(self.centre, fill = colArray[colourIndex])
 
     def __del__(self):
-        if self.midOval != 0:
-            canvas.delete(self.midOval)
+        if self.centre != 0:
+            canvas.delete(self.centre)
         if self.armBase != 0:
             canvas.delete(self.armBase)
         if self.armJoint != 0:
             canvas.delete(self.armJoint)
 
 
-
-    
-
 class lineSegments:
-    def __init__(self, xscreen, yscreen, line, oval):
+    def __init__(self, xscreen, yscreen, pathIndex):
         self.x = xscreen
         self.y = yscreen
-        self.line = line
-        self.oval = oval
+        if pathIndex == 0:
+            pastx = xscreen; pasty = yscreen
+        else:
+            pastx = drawPath[pathIndex-1].x
+            pasty = drawPath[pathIndex-1].y
+
+        self.line = canvas.create_line(xscreen, yscreen, pastx, pasty, width=lineThickness-1, fill='green')
+        
+        if pointHide == False:
+            self.oval = DrawPoint(xscreen, yscreen,0.8)
+        else: self.oval = []
 
     def __del__(self):
         if self.oval != 0:
             canvas.delete(self.oval)
         if self.line != 0:
             canvas.delete(self.line)
+    def hidePoint(self):
+        if self.oval != 0:
+            canvas.delete(self.oval)
+            self.oval = []
+    def drawPoint(self):
+        if self.oval == []:
+            self.oval = DrawPoint(self.x, self.y,0.8)
  
         
         
@@ -118,10 +129,13 @@ def UserDisplay_Callback(client,userdata,message):
 
             case "P":   # Case for path coords being sent
                 #print("Receiving path")
+                #AddDrawPoint(msgSplit)
+                #canvas.after(1, 
                 AddDrawPoint(msgSplit)
                 UpdateArms(msgSplit)
+                delay = 0.001 + 0.005 * (drawRateSlider.value)
                 
-                time.sleep(0.5)
+                time.sleep( delay)
 
             case "F":   # Case for path finish being transmitted
                 print("Path received, start drawing")
@@ -147,66 +161,43 @@ def RGBToHex(r,g,b):
 def AddDrawPoint(msg):
     x, y = CartToScreen(float(msg[2]) , float(msg[3]))
 
-    if drawPath != []:
-        index = len(drawPath) - 1
+    
         
-        line = canvas.create_line(x, y, drawPath[index].x, drawPath[index].y, width=lineThickness-1, fill='green')
-        oval = DrawPoint(x, y,0.8)
-
-        segment = lineSegments(x, y, line, oval)
-        drawPath.append(segment)
-    else:
-        
-        line = canvas.create_line(x, y, x,y, width=lineThickness-1, fill='green')
-        oval = DrawPoint(x, y,0.8)
-        segment = lineSegments(x, y, line, oval)
-        drawPath.append(segment)
+    i = len(drawPath)
+    segment = lineSegments(x, y, i)
+    drawPath.append(segment)
 
     
 
 def UpdateArms(msg):
-    global arms, colourList
+    global arms, colArray
     #print("Arm updating")
     endx, endy = CartToScreen(float(msg[2]) , float(msg[3]))
     midx, midy = CartToScreen(float(msg[4]) , float(msg[5]))
-    
-    if len(arms) != buffSize:
-        arms = []
-        colInc = [0,1,2]; colRGB = [0,1,2]
-        for i in colInc: colInc[i] = (endColour[i] - startColour[i]) / (buffSize-1)
+    try:
+        if len(arms) != buffSize:
+            arms.clear(); colArray.clear()  # Clear the arms and colour array
+            colInc = [0,1,2]; colRGB = [0,1,2]  # Prepare the colour incrementing arrays
+            for i in colInc: colInc[i] = (endColour[i] - startColour[i]) / (buffSize-1) # Populate colourInc with the RGB increments
 
-        for i in range(buffSize):
-            for j in range(len(colInc)): colRGB[j] = startColour[j] + (i * colInc[j])
-            colHex = RGBToHex(colRGB[0], colRGB[1], colRGB[2])
+            for i in range(buffSize):
+                for j in range(len(colInc)): colRGB[j] = startColour[j] + (i * colInc[j])
+                colHex = RGBToHex(colRGB[0], colRGB[1], colRGB[2])
+                colArray.append(colHex)
 
-            arm = canvas.create_line(endx, endy, midx, midy, width =lineThickness-1, fill = colHex)
-            base = canvas.create_line(midx, endy, visualiserArea/2, visualiserArea/2, width =lineThickness-1, fill = colHex)
-            centre = DrawPoint(midx, midy, 0.5)
+                struc = armStructure(endx, endy, midx, midy, i)
+                arms.append(struc)
 
-            struc = armStructure(base, arm, centre, colHex, i)
+        else:
+            for i in range(len(arms) - 2): # Shift the arms through the buffer and update their colours
+                arms[i+1].recolour(i)
+
+            arms.pop(0)
+            struc = armStructure(endx, endy, midx, midy, buffSize-1)
             arms.append(struc)
-            arms[i].undraw()
 
-    else:
-        for i in range(len(arms) - 1): # Shift the arms through the buffer and update their colours
-            
-            try:    # Catches errors occuring when resetting the drawing midway
-                if arms[i].buffNum == 0:    # If target line has reached end of buffer
-                    # Replace old arm with new arm
-                    arm = canvas.create_line(endx, endy, midx, midy, width =lineThickness-1, fill = arms[buffSize - 1].colour)
-                    base = canvas.create_line(midx, midy, visualiserArea/2, visualiserArea/2, width =lineThickness-1, fill = arms[buffSize - 1].colour)
-                    centre = DrawPoint(midx, midy, 0.5)
+    except: arms.clear()
 
-                    arms[i].replace(arm, base, centre, buffSize - 2)    # Set buffer to start counting down again
-                else:
-                    arms[i].buffNum -= 1
-                    arms[i].recolour(arms[ arms[i].buffNum ].colour)
-            except: 
-                    try:    # Attempts to delete any partly generated arms during the reset process
-                        canvas.delete(arm)
-                        canvas.delete(base)
-                        canvas.delete(centre)
-                    except: pass    # No left over lines to delete
 
 
 def DrawPoint(xscreen,yscreen, sizeMultiplier):
@@ -240,51 +231,61 @@ def CreateEntry(label ,relx , rely , widthScale, heightScale):
 
 class slider:
     def __init__(self, label, relx, rely, widthScale, heightScale, min, max):
-        self.label = label; self.min = min; self.max = max
-        self.sizex = controlArea / widthScale; self.sizey = visualiserArea / heightScale
+        sizex = controlArea / widthScale; sizey = visualiserArea / heightScale
+        self.max = max; self.min = min
         x, y = RelToControl(relx, rely)
-        self.posx = x - self.sizex/2
-        self.posy = y - self.sizey/2
+        self.posx = x - sizex/2
+        self.posy = y - sizey/2
+        self.value = min
 
-        self.widget = Scale(root, label=label, from_=min, to=max, length=self.sizex, showvalue=0, tickinterval=2, orient='horizontal', resolution=0.01, command= self.callback)
-        self.widget.place(x = self.posx, y = self.posy, width = self.sizex, height= self.sizey/2)
+        self.widget = Scale(root, label=label, from_=min, to=max, length=sizex, showvalue=0, tickinterval=2, orient='horizontal', resolution=0.01, command= self.callback)
+        self.widget.place(x = self.posx, y = self.posy, width = sizex, height= sizey/2)
         self.Title = Label(root, text="")
-        self.Title.place(x = self.posx, y = self.posy + self.sizey/2, width = self.sizex, height= self.sizey/4)
+        self.Title.place(x = self.posx, y = self.posy + sizey/2, width = sizex, height= sizey/4)
         print("Finished creating slider")
 
-
-    def callback(self, val):
+    def callback(self, val):    # Update the value of the slider and its text display
         self.Title.config(text='You have selected: ' + val)
-
+        self.value = float(val)
         # Set value as desired
+    
 
     
 # Convert relative x and y coordinates to their relative values in the control area
 def RelToControl(x, y):
     conx = (( 0.8 + (x * 0.2) ) * canvasSize)
     cony = (y * visualiserArea)
-  
-
     return conx, cony
     
-def SendRose():
+def SendRose(): # Send the values to generate a rose curve to the pi
     print("Sending Rose Variables")
     d = entryRosed.get()
     n = entryRosen.get()
+    res = str(resolutionSlider.value)
 
-    msg = str("P,Rose," + n + "," + d)
+    msg = str("P,Rose," + res + "," + n + "," + d)
     client.publish(user_topic, msg)
 
-def ResetDisplay():
+def ResetDisplay(): # Reset the display by deleting the path and arms
     global drawPath, arms
     print("Reset Display")
 
-    drawPath = []
-    arms = []
+    drawPath.clear()
+    arms.clear()
 
-def ResendPattern():
+def ResendPattern(): # Request the pi to resend the last path
     client.publish(user_topic, "P,Resend")
 
+def HidePoints():   # Toggles the visibility of the points on the display
+    global pointHide
+    pointHide = not pointHide
+    if pointHide == True:
+        for i in range(len(drawPath)):
+            drawPath[i].hidePoint()
+    else:
+        for i in range(len(drawPath)):
+            drawPath[i].drawPoint()
+        
 
 ## Main Start
 
@@ -305,16 +306,20 @@ canvas.create_line(visualiserArea, 0, visualiserArea, visualiserArea * 1.1, widt
 
 # Create User Buttons
 
-btnReset = CreateButton('Reset Display', ResetDisplay, 1/4, 1/3, 2.5, 20)
-btnResend = CreateButton('ResendPattern', ResendPattern, 3/4, 1/3, 2.5, 20)
+btnReset = CreateButton('Reset Display', ResetDisplay, 1/4, 26/100, 2.5, 20)
+btnResend = CreateButton('ResendPattern', ResendPattern, 3/4, 26/100, 2.5, 20)
+btnHidePoints = CreateButton('HidePoints', HidePoints, 3/4, 32/100, 2.5, 20)
 
 # Create rose n and d entry boxes
 entryRosen, titleRosen  = CreateEntry( "n", 1/12, 1/2, 7, 20)
 entryRosed, titleRosed = CreateEntry( "d", 3/12, 1/2, 7, 20)
 btnRoseSend = CreateButton('Send Rose', SendRose, 3/4, 1/2, 2.5, 20)
 
+
+
 # Creating Sliders
-drawRateSlider = slider("Test Slider", 1/2, 1/10, 1.2, 10, 0.1, 10)
+drawRateSlider = slider("Draw Speed", 1/2, 1/10, 1.05, 10, 0, 10)
+resolutionSlider = slider("Point Resolution (1x to 20x)", 1/2, 2/10, 1.05, 10, 1, 20)
 
 
 # Connecting to MQTT
