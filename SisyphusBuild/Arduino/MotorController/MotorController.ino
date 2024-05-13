af69fed398b9b894e2beef4047190aacc278bf0d
EoinBrennan2000 <eoinbrennan2000@gmail.com>
2024-05-13 18:48:38 +0100

    Update MotorController.ino
    Moved angle to steps conversion to the pi motor controller

--- SisyphusBuild/Arduino/MotorController/MotorController.ino
+++ SisyphusBuild/Arduino/MotorController/MotorController.ino
@@ -39,7 +39,6 @@ void setup() {
 void loop() {
   // Check for data in serial buffer
   while (Serial.available() > 0){
-    Serial.write("Reading\n");
     // Read next character from serial and respond
     char in = Serial.read();
     if (in != '\n') {
@@ -55,8 +54,15 @@ void loop() {
     }  
   }
   if ((moveStatus == true) && (targetSteps[0] != -1)){
-    StepsControl(targetSteps[0], targetSteps[1], 1000);
+    StepsControl(targetSteps[0], targetSteps[1], 3000);
   }
+  else if ((targetSteps[0] == -1) && (targetSteps[2] != -1)){
+    targetSteps[0] = targetSteps[2];
+    targetSteps[1] = targetSteps[3];
+    targetSteps[2] = -1;
+    targetSteps[3] = -1;
+  }
+  
 }
 
 void MessageHandler(char msg[50]){
@@ -70,9 +76,9 @@ void MessageHandler(char msg[50]){
       char *targetArm = strtok(NULL, ",");
       float angBase = atof(targetBase);
       float angArm = atof(targetArm);
-      // Convert to step equivalent and store
-      targetSteps[2] = (angBase / 0.9) + 0.5;
-      targetSteps[3] = (angArm / 0.9) + 0.5;
+      // Convert any negative steps to positive
+      if (targetSteps[2] < 0){ targetSteps[2] += 400; }
+      if (targetSteps[3] < 0){ targetSteps[3] += 400; }
       moveStatus = true;
       break;
     }
@@ -87,7 +93,9 @@ void MessageHandler(char msg[50]){
       
       // Convert to step equivalent and store
       targetSteps[0] = (angBase / 0.9) + 0.5;
+      if (targetSteps[0] < 0){ targetSteps[0] += 400; }
       targetSteps[1] = (angArm / 0.9) + 0.5;
+      if (targetSteps[1] < 0){ targetSteps[1] += 400; }
       moveStatus = false;
       break;
     }
@@ -97,11 +105,9 @@ void MessageHandler(char msg[50]){
     }
     default:{ // Back up response
       // Clear message and move on
-      Serial.println("Default Response");
       break;
     }
   }
-  Serial.println("Exiting System");
   
 }
 
@@ -117,7 +123,7 @@ void HomeRoutine(){
   // First sweep test
   for (inc = 1; inc <=400; inc++){
     stepperBase.step(1);
-    delay(800);
+    delay(60);
     sensorReading = analogRead(baseSensor);
     
     if (sensorReading > largestReading){
@@ -125,18 +131,18 @@ void HomeRoutine(){
       largestReading = sensorReading;
       largestPosition = inc;
     }
-    Serial.println(String("Position and value:" ) + inc + ":" + sensorReading);
+    //Serial.println(String("Position and value:" ) + inc + ":" + sensorReading);
   }
   // Move to the largest position
   stepperBase.step(largestPosition);
-  Serial.println(largestPosition);
+  //Serial.println(largestPosition);
 
   //Reset variables and perform second sweep
   largestReading = 512;
   largestPosition = 0;
   for (inc = 1; inc <=50; inc++){
     stepperJoint.step(1);
-    delay(20);
+    delay(60);
     sensorReading = analogRead(endSensor);
     
     if (sensorReading > largestReading){
@@ -153,48 +159,82 @@ void HomeRoutine(){
 // Function to opreate two motors at once
 void StepsControl(int stepsBase, int stepsJoint, int timeDelay)
   {
-    int stepsTotal = abs(stepsBase * stepsJoint);
-    int timeDiv = timeDelay / stepsTotal;
+    int baseMove, jointMove;
+    // Get the number of steps needed to reach the 
+    baseMove = stepsBase - currSteps[0];
+    jointMove = stepsJoint - currSteps[1];  
+
+    // Correct for movements gerater than half the circle
+    if (baseMove < -200) { baseMove += 400; }
+    else if (baseMove > 200) { baseMove -= 400;  }
+
+    if (jointMove < -200) { jointMove += 400; }
+    else if (jointMove > 200) { jointMove -= 400;  }
+    
+    
+    int stepsTotal = abs(baseMove * jointMove);
+    int timeDiv;
+    String msg;
     
     int baseDir, jointDir, i, timeStart, currTime;
-    if (stepsBase > 0) { baseDir = 1; }
+    if (baseMove > 0) { baseDir = 1; }
     else { baseDir = - 1; }
 
-    if (stepsJoint > 0) { jointDir = 1;}
+    if (jointMove > 0) { jointDir = 1;}
     else { jointDir = - 1; }
 
 
-    if (stepsJoint == 0) // Rotate base only
+    if (jointMove == 0) // Rotate base only
     {
-      stepperBase.step(stepsBase);
+      timeDiv = abs(timeDelay/baseMove);
+      if (timeDiv < 1){timeDiv = 1;}
+      msg = String("timeDiv in jointMove 0: ") + timeDiv;
+      //Serial.println(msg);
+      for (i = 1; i <= abs(baseMove); i++){
+        stepperBase.step(baseDir);
+        delay(timeDiv);
+      }
     }
-    else if (stepsBase == 0) // Rotate joint only
+    else if (baseMove == 0) // Rotate joint only
     {
-      stepperJoint.step(stepsJoint);
+      
+      timeDiv = abs(timeDelay/jointMove);
+      if (timeDiv < 1){timeDiv = 1;}
+      msg = String("timeDiv in baseMove 0: ") + timeDiv;
+      //Serial.println(msg);
+      for (i = 1; i <= abs(jointMove); i++){
+        stepperJoint.step(jointDir);
+        delay(timeDiv);
+      }
     }
     else {
+      timeDiv = timeDelay / stepsTotal;
+      if (timeDiv < 1){timeDiv = 1;}
+      msg = String("timeDiv in both move: ") + timeDiv;
+      //Serial.println(msg);
       
       for (i = 1; i <= stepsTotal; i++) 
       {
-        timeStart = millis();
 
         if (i % stepsJoint == 0) {
           stepperBase.step(baseDir);
         }
         if (i % stepsBase == 0) {
-          stepperJoint.step(baseDir);
+          stepperJoint.step(jointDir);
         }
 
-        currTime = millis();
-        while (currTime - timeStart < timeDiv) {
-          currTime = millis();
-        }
+        delay(timeDiv);
         
       }
     }
     // Shifting position store buffer and confirming completion
+    currSteps[0] = stepsBase;
+    currSteps[1] = stepsJoint;
+    
     targetSteps[0] = targetSteps[2];
     targetSteps[1] = targetSteps[3];
     targetSteps[2] = -1; targetSteps[3] = -1;
+    
+    //delay(timeDelay);
     Serial.write("C\n");
   }
